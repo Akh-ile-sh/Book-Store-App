dbd48c88a6e267c8f62758d43c03026fdca1df0d
Akhilesh C <akhileshcvdy@gmail.com>
2024-07-16 12:45:21 +0530

    create book page and showing book detail page is completed in frontend

--- client/src/Pages/ShowBook.jsx
+++ client/src/Pages/ShowBook.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const ShowBook = () => {
-  return <div>ShowBook</div>;
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3000/api/v1/books/getOneBook/${id}`)
+      .then((response) => {
+        setBook(response.data.book);
+
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4 ">showBook</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+          <div className="my-4">
+            <span className="text-xl font-bold mr-4 text-gray-500">Id</span>
+            <span>{book._id}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl font-bold mr-4 text-gray-500">Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl font-bold mr-4 text-gray-500">Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl font-bold mr-4 text-gray-500">Year</span>
+            <span>{book.publishedYear}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ShowBook;
